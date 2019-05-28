0a5b58b404155883eff1af0fb46ad77ced84d7d5
Anurag Bajaj <anuragb26@gmail.com>
2019-05-28 16:24:04 +0530

    Feat:Add JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const { registerValidation, loginValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
@@ -22,16 +23,34 @@ router.post("/register", async (req, res) => {
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashPassword
   });
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: user._id });
   } catch (e) {
     res.status(400).send(e);
   }
 });
-router.post("/login", (req, res) => {
-  res.send("Login");
+router.post("/login", async (req, res) => {
+  try {
+    const { error } = loginValidation(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).send("Email or password is not valid");
+    }
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) {
+      return res.status(400).send("Email or password is wrong");
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header("auth-token", token).send(token);
+    // res.send("Succcess");
+  } catch (error) {
+    console.log("err", error);
+  }
 });
 module.exports = router;
